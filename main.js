4759e2859b3879d6037fbd9048155aa0494509f1
Egor <68728836+egorf710@users.noreply.github.com>
2024-07-17 02:40:52 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function Follow(a)
+{
+    switch (a) {
+        case 1:
+            window.open("https://www.youtube.com/@ChewyBytesStudio");
+            break;
+        case 2:
+            window.open("https://www.tiktok.com/@chewybytesstudio");
+            break;
+        case 3:
+            window.open("https://www.instagram.com/chewybytesstudio/");
+            break;
+        case 4:
+            window.open("https://x.com/ChewyBytesS");
+            break;
+        default:
+            break;
+    }
+
+}
