854ccb8c88c62a96ae760e8a963658c577ae8ad0
Egor <68728836+egorf710@users.noreply.github.com>
2024-07-16 03:08:55 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+if (typeof window._phv2Activator === "undefined") {
+document.addEventListener('phv2site.ready', (e) => 
+{
+    window._phv2Activator.activate('native_web_push');
+});
+} 
+else 
+{
+    window._phv2Activator.activate('native_web_push');
+}
